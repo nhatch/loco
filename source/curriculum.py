193d9a15ee7fa7ea15e71b352566992e0e5c169e
Nathan Hatch <nathanhatch297@gmail.com>
2019-01-19 16:29:21 -0500

    Forgot to add the new file curriculum.py

---
+++ source/curriculum.py
@@ -0,0 +1,79 @@
+
+TRAIN_SETTINGS_2D = {
+    'n_dirs': 4,
+    'tol': 0.02,
+    'step_size': 0.1,
+    }
+
+SETTINGS_2D = {
+    'use_stepping_stones': True,
+    'dist_mean': 0.47,
+    'dist_spread': 0.3,
+    'ground_length': 0.1,
+    'ground_width': 0.5,
+    'n_steps': 16,
+    'z_mean': 0.0,
+    'z_spread': 0.0,
+    'y_mean': 0.05,
+    'y_spread': 0.1,
+    }
+
+TRAIN_SETTINGS_3D = {
+    'n_dirs': 8,
+    'tol': 0.05,
+    'step_size': 0.2,
+    }
+
+TRAIN_SETTINGS_3D_PRECISE = {
+    'n_dirs': 8,
+    'tol': 0.02,
+    'step_size': 0.1,
+    }
+
+SETTINGS_3D_EASY = {
+    'use_stepping_stones': False,
+    'dist_mean': 0.35,
+    'dist_spread': 0.0,
+    'n_steps': 16,
+    'z_mean': 0.4,
+    'z_spread': 0.0,
+    'y_mean': 0.0,
+    'y_spread': 0.0,
+    }
+
+SETTINGS_3D_MEDIUM = {
+    'use_stepping_stones': False,
+    'dist_mean': 0.35,
+    'dist_spread': 0.2,
+    'n_steps': 16,
+    'z_mean': 0.4,
+    'z_spread': 0.1,
+    'y_mean': 0.0,
+    'y_spread': 0.0,
+    }
+
+SETTINGS_3D_HARD = {
+    'use_stepping_stones': False,
+    'dist_mean': 0.35,
+    'dist_spread': 0.5,
+    'n_steps': 16,
+    'z_mean': 0.4,
+    'z_spread': 0.2,
+    'y_mean': 0.0,
+    'y_spread': 0.0,
+    }
+
+SETTINGS_3D_HARDER = {
+    'use_stepping_stones': True,
+    'dist_mean': 0.35,
+    'dist_spread': 0.5,
+    'ground_length': 0.3,
+    'ground_width': 0.2,
+    'n_steps': 16,
+    'z_mean': 0.4,
+    'z_spread': 0.2,
+    'y_mean': 0.0,
+    'y_spread': 0.0,
+    }
+
+
