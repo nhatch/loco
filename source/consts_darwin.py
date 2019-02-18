Fix heel location calculation for Z-axis gravity
Also make dot radius configurable (Darwin needs a smaller radius),
and add the relevant foot dimension constants to consts_darwin.py.

Also remove some more unused code from inverse_kinematics.py.

source/consts_darwin.py
@@ -6,6 +6,7 @@ from consts_common3D import *
 Q_DIM_RAW = 26
 SIMULATION_RATE = 0.002
 GROUND_LEVEL = -0.35
+DEFAULT_ZOOM = 2.0
 
 GRAVITY_Y = False
 
@@ -79,4 +80,9 @@ CONTROL_BOUNDS = np.array([-7.5*np.ones(20,), 7.5*np.ones(20,)])
 KP_GAIN = np.array([154.019]*20)
 KD_GAIN = np.array([0.1002]*20)
 
-DEFAULT_ZOOM = 2.0
+FOOT_RADIUS = 0.004
+L_FOOT = 0.104
+RIGHT_BODYNODE_IDX = 22
+LEFT_BODYNODE_IDX = 16
+FOOT_BODYNODE_OFFSET = 5
+DOT_RADIUS = 0.01

source/inverse_kinematics.py
@@ -13,27 +13,19 @@ class InverseKinematics:
         c = self.env.consts()
         if c.BRICK_DOF == 3:
             foot_centric_offset = np.array([-0.5*c.L_FOOT, -c.FOOT_RADIUS, 0.0])
-        else:
+        elif c.GRAVITY_Y:
             # I found the signs/order of these offsets by trial and error.
             # I think the reason it's different might be the axis_order setting
             # of 'zyx' in the skel file for the simple 3D model.
             foot_centric_offset = np.array([0.0, -c.FOOT_RADIUS, 0.5*c.L_FOOT])
+        else:
+            # Again, trial and error
+            foot_centric_offset = np.array([c.FOOT_RADIUS, 0.0, -0.5*c.L_FOOT])
         foot = self.get_bodynode(swing_idx, c.FOOT_BODYNODE_OFFSET)
         # TODO is it cheating to pull foot.com() directly from the environment?
         # TODO fix this for Z gravity!
         heel_location = np.dot(foot.transform()[:3,:3], foot_centric_offset) + foot.com()
-        return heel_location
-
-    def heading(self, swing_idx):
-        # We only ever use this in 3D
-        c = self.env.consts()
-        # The forward direction. Again, trial and error.
-        foot_centric_offset = np.array([0.0, 0.0, -1.0])
-        foot = self.get_bodynode(swing_idx, c.FOOT_BODYNODE_OFFSET)
-        direction = np.dot(foot.transform()[:3,:3], foot_centric_offset)
-        current_heading = self.env.controller.heading()
-        _, heading = heading_from_vector(direction, current_heading)
-        return heading
+        return c.inverse_convert_root(heel_location) # TODO
 
     def get_bodynode(self, swing_idx, offset):
         c = self.env.consts()
@@ -54,8 +46,7 @@ class InverseKinematics:
         self.env.reset(random=0.3)
         heel_loc = self.forward_kine(idx)
         self.env.sdf_loader.put_dot(heel_loc[:3], 'heel_loc', color=BLUE)
-        if self.env.is_3D:
-            self.env.pause()
+        self.env.pause()
 
     # Returns the values of the 6 "brick DOFs" for which, if the rest of the DOFs
     # remain the same, the given bodynode will have the given target transformation.
@@ -128,39 +119,19 @@ class InverseKinematics:
         print(np.allclose(target_root_transform[:3,:3], pelvis.transform()[:3,:3], atol=1e-6))
         self.env.pause(0.5)
 
-    def gen_brick_pose(self, planar):
-        # Generate a random starting pose and target.
-        D = self.env.consts().BRICK_DOF
-        center = np.zeros(D)
-        if D == 3:
-            scale = [0.2, 0.2, 0.2]
-        else:
-            scale = [0.2, 0.2, 0.2, np.pi/6, 2*np.pi, np.pi/6]
-            if planar:
-                non_planar_dofs = [2,4,5]
-                scale[non_planar_dofs] = 0.0
-        brick_pose = center + scale*np.random.uniform(low=-0.5, high=0.5, size=D)
-        target = center + np.random.uniform(low=-0.5, high=0.5, size=D)
-        if D == 3:
-            target[1] += 0.5
-            # Technically 2 represents the pitch, but here we're repurposing it as Z coordinate
-            target[2] = 0.0
-        else:
-            target[1] -= 0.5
-        return brick_pose, target[:3]
-
 if __name__ == "__main__":
+    from pd_control import PDController
     from stepping_stones_env import SteppingStonesEnv
     from simple_3D_env import Simple3DEnv
-    from simbicon_3D import Simbicon3D
+    from darwin_env import DarwinEnv
     #env = SteppingStonesEnv()
-    env = Simple3DEnv(Simbicon3D)
+    #env = Simple3DEnv(Simbicon3D)
+    env = DarwinEnv(PDController)
     env.track_point = [0,0,0]
     ik = InverseKinematics(env.robot_skeleton, env)
     ik.test()
-    embed()
     c = env.consts()
-    bodynode = env.robot_skeleton.bodynodes[c.RIGHT_BODYNODE_IDX+c.THIGH_BODYNODE_OFFSET]
+    #bodynode = env.robot_skeleton.bodynodes[c.RIGHT_BODYNODE_IDX+c.THIGH_BODYNODE_OFFSET]
     #ik.test_inverse_transform(bodynode)
-    ik.test_inverse_hip(c.RIGHT_IDX, heading=-1.0, pitch=0.2)
+    #ik.test_inverse_hip(c.RIGHT_IDX, heading=-1.0, pitch=0.2)
     embed()

source/sdf_loader.py
@@ -30,12 +30,15 @@ class SDFLoader:
         # Change the skeleton name so that the console output is not cluttered
         # with warnings about duplicate names.
         dot = self.dots.get(name)
+        radius = self.consts.DOT_RADIUS
         if dot is None:
             os.system("sed -e 's/__NAME__/dot_{}/' skel/dot.sdf > skel/_dot{}.sdf".format(
                 name, UQ_ID))
             os.system("sed -e 's/__COLOR__/{}/' skel/_dot{}.sdf > skel/__dot{}.sdf".format(
                 color, UQ_ID, UQ_ID))
-            dot = self.world.add_skeleton('./skel/__dot{}.sdf'.format(UQ_ID))
+            os.system("sed -e 's/__RADIUS__/{}/' skel/__dot{}.sdf > skel/___dot{}.sdf".format(
+                radius, UQ_ID, UQ_ID))
+            dot = self.world.add_skeleton('./skel/___dot{}.sdf'.format(UQ_ID))
             self.dots[name] = dot
             dot.set_root_joint_to_trans_and_euler()
         dot.q = self.consts.convert_root(np.concatenate([target, [0,0,0]]))