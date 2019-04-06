f6cb55d7a3f4a7ceebdb56668c220ef7b8bdf6de
Nathan Hatch <nathanhatch297@gmail.com>
2019-04-06 17:13:09 -0400

    Overhaul learn_llc objective function / controllable parameters
    Also, save learning data in data/ to be able to monitor learning
    progress more objectively and save the trained parameter settings.
    
    The objective function tweaks were an attempt to get a better-performing
    controller. (Apparently adding joint limits and removing target
    observations / foot flattening made the previous objective
    insufficient.) However, now I'm worried that it's overspecified; I'll
    try to simplify it in future commits.
    
    It's still not good enough to train a walking controller. I'm having the
    problem that the robot is unable to hold its torso upright and
    inevitably eventually falls over (forward or backward).

--- source/learn_llc.py
+++ source/learn_llc.py
@@ -12,17 +12,14 @@ import utils
 
 controllable_params = utils.build_mask(sp.N_PARAMS,
         [
-        #sp.IK_GAIN,
-        sp.UP_DURATION,
         sp.TORSO_WORLD,
         sp.STANCE_HIP_ROLL_EXTRA,
         sp.STANCE_ANKLE_RELATIVE,
         sp.UP_IDX+sp.SWING_HIP_WORLD,
         sp.UP_IDX+sp.SWING_KNEE_RELATIVE,
         sp.UP_IDX+sp.STANCE_KNEE_RELATIVE,
-        sp.DN_IDX+sp.SWING_HIP_WORLD,
-        sp.DN_IDX+sp.SWING_KNEE_RELATIVE,
-        sp.DN_IDX+sp.STANCE_KNEE_RELATIVE,
+        sp.POSITION_BALANCE_GAIN,
+        sp.VELOCITY_BALANCE_GAIN,
         ])
 
 def test(env, length, param_setting, render=None, n=100, terminate_on_slip=True):
@@ -30,37 +27,40 @@ def test(env, length, param_setting, render=None, n=100, terminate_on_slip=True)
     seed = np.random.randint(100000)
     env.reset(seed=seed, random=0.005, video_save_dir=None, render=render)
     env.sdf_loader.put_grounds([[-3.0, GL, 0]])
-    prev_stance_heel = env.controller.stance_heel
-    t = prev_stance_heel.copy()
-    t[2] = 0.0
+    c = env.controller
     penalty = 0.0
     for i in range(n):
-        l = length*0.5 if i == 0 else length
-        #t[0] = prev_stance_heel[0]
-        t[0] += l
-        obs, terminated = env.simulate(t, target_heading=0.0, action=param_setting, put_dots=True)
+        stance_com = foot_center(env, c.stance_idx)
+        obs, terminated = env.simulate(np.zeros(3), target_heading=0.0, action=param_setting,
+                put_dots=True)
         if terminated:
             penalty += 100.0
-        new_stance_heel = env.controller.stance_heel
-        penalty += np.abs(new_stance_heel[0] - t[0])
-        slip_dist = slipping(env, prev_stance_heel)
+        penalty += 0.5*(c.stance_heel[1] - GL) # Encourage heelstrikes, not toestrikes
+        slip_dist = slipping(env, stance_com)
         if render is not None:
             print("Slip:", slip_dist)
+        if i == 0:
+            slip_dist /= 2 # First steps are weird, don't penalize as heavily
         penalty += slip_dist
         if slip_dist > MAX_SLIPPING and terminate_on_slip:
             terminated = True
         if terminated:
             break
-        prev_stance_heel = new_stance_heel
-    dist = prev_stance_heel[0]
+    penalty += np.abs(c.stance_heel[0] - i*length)
+    dist = i*length
     if render is not None:
         print("Distance achieved:", dist, "Penalty:", penalty)
     return -dist + penalty
 
-def slipping(env, prev_stance_heel):
+def foot_center(env, idx):
     c = env.controller
-    curr_swing_heel = c.ik.forward_kine(c.swing_idx)
-    slip_distance = np.linalg.norm(prev_stance_heel - curr_swing_heel)
+    foot = c.ik.get_bodynode(idx, env.consts().FOOT_BODYNODE_OFFSET)
+    return foot.com()
+
+def slipping(env, prev_stance_com):
+    c = env.controller
+    curr_swing_com = foot_center(env, c.swing_idx)
+    slip_distance = np.linalg.norm(prev_stance_com - curr_swing_com)
     return slip_distance
 
 def init_opzer(env, init_mean):
@@ -76,37 +76,48 @@ means = []
 vals = []
 
 def learn(opzer, n_iters):
-    for i in range(n_iters):
-        val = opzer.f(opzer.mean, render=4)
-        means.append(opzer.mean)
-        vals.append(val)
-        print("Iteration", i, ":", val)
-        opzer.iter()
+    try:
+        for i in range(n_iters):
+            val = opzer.f(opzer.mean, render=4)
+            means.append(opzer.mean)
+            vals.append(val)
+            graph(vals, opzer)
+            print("Iteration", i, ":", val)
+            opzer.iter()
+    except SystemExit: # Pydart for some dumb reason turns keyboard interrupts into this
+        return
+
+def graph(vals, opzer):
+    import matplotlib.pyplot as plt
+    plt.plot(vals)
+    root = 'data/learn_llc/'
+    plt.savefig(root+'learning_curve.pdf')
+    plt.clf()
+    np.savetxt(root+'mean.txt', opzer.mean)
 
 b0 = np.zeros((controllable_params.sum(),1))
+b1 = np.array(
+      [[-0.33859163],
+       [-0.13842327],
+       [ 0.6797675 ],
+       [ 0.30470689],
+       [ 1.57372148],
+       [ 0.22417648],
+       [0],
+       [0],
+       ])
 
-# Best so far: observes target
-# Works only if you check out commit 6e2c0667db5df3b42548ac8b14a47aeea5929826
-# (use these same numbers though)
-b2 = np.array([[-0.18549841],
-       [-0.06580528],
-       [ 0.15634049],
-       [-0.37124493],
-       [ 0.97356754],
-       [-0.05797221],
-       [-0.19359471]])
-
-# Best so far: does not observe target (problem: knees don't obey joint limits)
-b3 = np.array([[-0.12618084],
-       [ 0.227121  ],
-       [-0.09054774],
-       [ 0.35178088],
-       [ 0.27585969],
-       [-0.1552619 ],
-       [ 0.63135911],
-       [ 0.01060128],
-       [ 0.07810596],
-       [-0.01016673]])
+# Most recent trained gait; adjusts balance gains
+b2 = np.array([
+-1.876361897471146567e-01,
+-3.267255715298817975e-01,
+-1.224211771795346709e-01,
+3.866096294088597896e-01,
+-5.428567881104896103e-02,
+6.453115104200566332e-01,
+-1.543063808195492270e-01,
+3.479733144470131267e-01,
+]).reshape((-1, 1))
 
 
 if __name__ == "__main__":
@@ -116,6 +127,6 @@ if __name__ == "__main__":
     p = np.zeros(len(controllable_params))
     #test(env, REFERENCE_STEP_LENGTH, p, r=8)
     opzer = init_opzer(env, b0)
-    opzer.f(b3, render=2, terminate_on_slip=False)
+    #opzer.f(b3, render=2, terminate_on_slip=False)
     #learn(opzer, 40)
     embed()
