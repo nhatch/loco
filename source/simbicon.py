nhatch/loco

commit 56103b350973f9ff6048bcb2f3f76ce207ebb4bf

Tweak "fell off" detection: use targets, not heel locations
Also remove some unused code.

source/simbicon.py
@@ -167,14 +167,14 @@ class Simbicon(PDController):
         #   python inverse_dynamics.py     # Don't run any training iters or load the train set
         #   learn.evaluate(seed=39738)
         tol = -0.06
-        lower = min(self.starting_swing_heel[1], self.target[1])
-        upper = max(self.starting_swing_heel[1], self.target[1])
+        lower = min(self.prev_target[1], self.target[1])
+        upper = max(self.prev_target[1], self.target[1])
         below_lower = swing_heel[1] - lower < tol
         below_upper = swing_heel[1] - upper < tol
         # Calculate the distance of the swing heel from the line between the
         # start and end targets.
         # Technically in 3D this is a plane, not a line.
-        below_line = np.dot(swing_heel - self.starting_swing_heel, self.unit_normal) < tol
+        below_line = np.dot(swing_heel - self.prev_target, self.unit_normal) < tol
         if below_lower or (below_line and below_upper):
             print("FELL OFF")
             return True
@@ -245,6 +245,9 @@ class Simbicon(PDController):
         tq[self.swing_idx+c.ANKLE:self.swing_idx+c.ANKLE+ANKLE_DOF] = self.ik.get_ankle(self.swing_idx)
         tq[self.stance_idx+c.ANKLE:self.stance_idx+c.ANKLE+ANKLE_DOF] = self.ik.get_ankle(self.stance_idx)
         tq[self.swing_idx+c.ANKLE] += params[sp.SWING_ANKLE_RELATIVE+DIR_IDX]
+        # Note = not +=. This overwrites parallel-to-ground correction for stance ankle.
+        # I guess I'd rather not do this, but fixing this "bug" breaks the controller
+        # for the simple 2D and 3D models.
         tq[self.stance_idx+c.ANKLE] = params[sp.STANCE_ANKLE_RELATIVE]
 
         # This code is only useful in 3D.
@@ -286,8 +289,8 @@ class Simbicon(PDController):
         # in order to prevent the robot from stepping so hard that it bounces.
         fix_Kd = (not self.env.is_3D) and self.direction == UP \
                 and self.time() - self.step_started < 0.1
-        fix_Kd_idx = c.fix_Kd_idx(self.stance_idx)
         if fix_Kd:
+            fix_Kd_idx = c.fix_Kd_idx(self.stance_idx)
             self.Kd[fix_Kd_idx] *= 8
         raw_control = self.compute_PD()
         if fix_Kd:

source/consts_armless.py
@@ -46,14 +46,6 @@ def virtual_torque_idx(standardized_idx):
     else:
         raise "Invalid stance index"
 
-def fix_Kd_idx(standardized_idx):
-    if standardized_idx == RIGHT_IDX:
-        return 15
-    elif standardized_idx == LEFT_IDX:
-        return 9
-    else:
-        raise "Invalid stance index"
-
 CONTROL_BOUNDS = 1000 * np.array([[-100]*Q_DIM, [100]*Q_DIM])
 
 PELVIS_BODYNODE_IDX = 2

source/consts_darwin.py
@@ -70,14 +70,6 @@ def virtual_torque_idx(standardized_idx):
     else:
         raise "Invalid stance index"
 
-def fix_Kd_idx(standardized_idx):
-    if standardized_idx == RIGHT_IDX:
-        return 23
-    elif standardized_idx == LEFT_IDX:
-        return 17
-    else:
-        raise "Invalid stance index"
-
 CONTROL_BOUNDS = np.array([-7.5*np.ones(20,), 7.5*np.ones(20,)])
 
 KP_GAIN = np.array([154.019]*20)

source/skel/darwinmodel/doppelganger.urdf
@@ -34,7 +34,7 @@
   <joint name="base_link_to_body" type="fixed">
     <parent link="base_link"/>
     <child link="MP_BODY"/>
-    <origin rpy="0 0 0" xyz="0.1 -0.5 0"/>
+    <origin rpy="0 0 0" xyz="0.1 -0.5 0.1"/>
   </joint>
   
   <link name="MP_BACK_L">

commit 2ee4424effc97c8b9ab3494c69c538c396326760

Refactor crash detection again
The commit 56103b35 introduced a bug that I didn't catch until now. Now,
the Simbicon controller tracks all three recent targets during the
course of the step, and only rotates the new swing/stance platforms
after change_stance is called.

source/simbicon.py
@@ -37,29 +37,29 @@ class Simbicon(PDController):
             self.swing_idx = c.RIGHT_IDX
             self.stance_idx = c.LEFT_IDX
             self.stance_heel = self.ik.forward_kine(self.stance_idx)
-            self.target = self.stance_heel
+            self.stance_platform = self.stance_heel
             swing_heel = self.ik.forward_kine(self.swing_idx)
-            self.prev_target = swing_heel
+            self.swing_platform = swing_heel
+            self.target = swing_heel # For Darwin's sake. This is never set, otherwise.
         else:
             self.swing_idx = c.LEFT_IDX if state.swing_left else c.RIGHT_IDX
             self.stance_idx = c.RIGHT_IDX if state.swing_left else c.LEFT_IDX
             self.stance_heel = state.stance_heel_location()
-            # The method `set_gait_raw` must also be called before simulation continues,
-            # in order to set a new target and gait.
-            # At that point, we will rotate self.target to self.prev_target.
-            self.target = state.stance_platform()
-            # We track prev_target so that, in principle, we know all of the possible
+            self.stance_platform = state.stance_platform()
+            # We track swing_platform so that, in principle, we know all of the possible
             # places where the agent is in contact with the environment. However, the
             # algorithm does not use this (yet) except when resetting the environment
             # to a previously visited state.
-            self.prev_target = state.swing_platform()
+            self.swing_platform = state.swing_platform()
+        # The method `set_gait_raw` must also be called before simulation continues,
+        # in order to set a target and gait.
 
     def state(self):
         # TODO make State attributes settable, so we can construct this like
-        # state.stance_platform = self.target
+        # state.stance_platform = self.stance_platform
         # Until then, make sure that this order corresponds to the indices defined
         # in the State object.
-        return np.concatenate((self.stance_heel, self.target, self.prev_target))
+        return np.concatenate((self.stance_heel, self.stance_platform, self.swing_platform))
 
     def heading(self):
         return 0.0
@@ -76,7 +76,7 @@ class Simbicon(PDController):
             return # Skip the rest of setup
 
         swing_heel = self.ik.forward_kine(self.swing_idx)
-        self.target, self.prev_target = target, self.target
+        self.target = target
         d = self.target - swing_heel
 
         if target_heading is None:
@@ -141,19 +141,16 @@ class Simbicon(PDController):
         # For some reason, setting the tolerance smaller than .05 or so causes the controller
         # to learn very weird behaviors. TODO: why does this have such a large effect??
         # However, setting the tolerance too large (larger than .04 or so) makes certain crashes
-        # go undetected. E.g.:
-        #
-        #   python inverse_dynamics.py     # Don't run any training iters or load the train set
-        #   learn.evaluate(seed=39738)
+        # go undetected.
         tol = -0.06
-        lower = min(self.prev_target[1], self.target[1])
-        upper = max(self.prev_target[1], self.target[1])
+        lower = min(self.swing_platform[1], self.target[1])
+        upper = max(self.swing_platform[1], self.target[1])
         below_lower = swing_heel[1] - lower < tol
         below_upper = swing_heel[1] - upper < tol
         # Calculate the distance of the swing heel from the line between the
         # start and end targets.
         # Technically in 3D this is a plane, not a line.
-        below_line = np.dot(swing_heel - self.prev_target, self.unit_normal) < tol
+        below_line = np.dot(swing_heel - self.swing_platform, self.unit_normal) < tol
         if below_lower or (below_line and below_upper):
             print("FELL OFF")
             return True
@@ -190,6 +187,7 @@ class Simbicon(PDController):
         return early_strike
 
     def change_stance(self, swing_heel):
+        self.swing_platform, self.stance_platform = self.stance_platform, self.target
         self.step_started = self.time()
         self.stance_heel = swing_heel
         self.direction = UP